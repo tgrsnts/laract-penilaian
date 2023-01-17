34a0508820853f86199d3180c8e634b52d837def
tgrsnts <tegarsantoso72@gmail.com>
2023-01-17 15:52:35 +0700

    .

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -10,40 +10,42 @@
 </head>
 
 <body>
-    {{-- HEADER --}}
-    <div class="header">
-        <img src="{{ asset('/gambar/header.jpg') }}" alt="">
-    </div>
+    <div>
+        {{-- HEADER --}}
+        <div class="header">
+            <img src="{{ asset('/gambar/header.jpg') }}">
+        </div>
 
-    {{-- MENU  --}}
-    <div class="menu">
-        <b>
-            <a href="/home">HOME</a>
-            {{-- @if (session('user')->role == 'admin') --}}
+        {{-- MENU  --}}
+        <div class="menu">
+            <b>
+                <a href="/home">HOME</a>
+                {{-- @if (session('user')->role == 'admin') --}}
                 <a href="/guru/index">GURU</a>
                 <a href="/jurusan/index">JURUSAN</a>
                 <a href="/kelas/index">KELAS</a>
                 <a href="/siswa/index">SISWA</a>
                 <a href="/mapel/index">MAPEL</a>
                 <a href="/mengajar/index">MENGAJAR</a>
-            {{-- @else --}}
-            <a href="/nilai/index">NILAI</a>
-            {{-- @endif --}}
-            <a href="/logout">LOGOUT</a>
-        </b>
-    </div>
-    {{-- CONTENT  --}}
-    @yield('content')
+                {{-- @else --}}
+                <a href="/nilai/index">NILAI</a>
+                {{-- @endif --}}
+                <a href="/logout">LOGOUT</a>
+            </b>
+        </div>
+        {{-- CONTENT  --}}
+        @yield('content')
 
-    {{-- FOOTER  --}}
-    <div class="footer">
-        <center>
-            <p>
-                &copy; {{ date('Y')}} - UJIKOM LSP
-            </p>
-        </center>
-    </div>  
+        {{-- FOOTER  --}}
+        <div class="footer">
+            <center>
+                <p>
+                    &copy; {{ date('Y') }} - UJIKOM LSP
+                </p>
+            </center>
+        </div>
+    </div>
 
 </body>
 
-</html>l
+</html>
